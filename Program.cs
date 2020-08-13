3722195cb68fab3eda1cf578b6030c75c17b7b15
Christine Echo <christineecho@outlook.com>
2020-08-13 16:16:53 -0600

    Adds pseudocode;

--- Program.cs
+++ Program.cs
@@ -11,6 +11,23 @@ namespace c_odd_even_sorter_practice_KrisztinaPap
                 Purpose: The program prompts the user for an array of ten numbers and returns an array of the even numbers from lowest to highest followed by the odd numbers from lowest to highest. 
                 Author: Krisztina Pap
                 Date of last edit: Aug 13, 2020
+            */
+
+        /*
+            -- Explain program to user
+            -- Declare variables
+            -- Create empty number array, initilize all values at 0
+            -- Prompt user for the ten numbers 
+            -- Sort numbers by even/odd and then lowest to highest using methods
+            -- Return array 
+
+
+            Challenges:
+            -- validate numbers entered using a method (must be integers or the word 'done' or it will throw error and prompt again)
+            -- Solve the problem using LINQ or a lambda expression in Sort()
+            --Possible unique features:
+                -- 
+
         */
         }
     }
