dc7440cbb31a579ecb20f7a351792952b2cb496e
Christine Echo <christineecho@outlook.com>
2020-08-13 17:40:44 -0600

    Adds sentinel value loop - Huge shout-out to Aaron Champagne for his troubleshooting help!

--- Program.cs
+++ Program.cs
@@ -13,11 +13,41 @@ namespace c_odd_even_sorter_practice_KrisztinaPap
                 Date of last edit: Aug 13, 2020
             */
 
+        // Explains program to the user
+        Console.WriteLine("Welcome to Number-sorter! You will be prompted for up to ten numbers and we will give you a sorted list with even numbers first, from lowest to highest, followed by the odd numbers, from lowest to highest. Just enter 'done' if you don't want to enter any more numbers. Hit enter when you are ready to start!");
+        Console.ReadLine(); // Waits for user to continue
+
+        // Declare variables
+        string userInput = "";
+
+        // Creates an empty number array, initilizes all values at 0
+        int[] numbers = new int[10];
+        Console.WriteLine(numbers[9]);
+        int i = 0;
+        // Sentinel loop with 'done' as the breaking value
+        while ( userInput != "done" && i < 10 )
+        {
+            // Populates array
+            // for ( int i = 0; i < numbers.Length; i++ )
+            // {
+                Console.WriteLine("Enter number {0}:", i+1);
+                userInput = Console.ReadLine().ToLower();
+                if (userInput != "done")
+                {
+                    numbers[i] = int.Parse(userInput);
+                    i++;
+                }
+                
+            // }
+        }         
+
+                 
+
+
         /*
-            -- Explain program to user
             -- Declare variables
-            -- Create empty number array, initilize all values at 0
-            -- Prompt user for the ten numbers 
+            
+            
             -- Sort numbers by even/odd and then lowest to highest using methods
             -- Return array 
 
